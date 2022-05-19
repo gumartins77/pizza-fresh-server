b163fb79d769ddc19861c8849c6f1b450d0e9260
Gustavo Martins <gugumartin77@gmail.com>
2022-05-18 21:51:51 -0300

    feat(table): add `create` endpoint

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('table')
 export class TableController {
   @Get()
   findAll() {
     return 'Buscar todas as mesas';
+  }
+
+  @Post()
+  create() {
+    return 'Criar uma mesa';
   }
 }
