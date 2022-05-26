babb4fd7e81a0717758c1da436ecdd754f451c28
Gustavo Martins <gugumartin77@gmail.com>
2022-05-26 12:19:41 -0300

    feat(product): implement dto & service

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,6 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Body,
+  HttpException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  create(dto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findById(id: string): Promise<Product> {
+    const record = await this.prisma.product.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Record with Id '${id}' not found!`);
+    }
+
+    return record;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string): Promise<Product> {
+    return this.findById(id);
   }
 
-  update(id: string, dto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  create(dto: CreateProductDto): Promise<Product> {
+    const data: Product = { ...dto };
+
+    return this.prisma.product.create({ data }).catch(this.handleError);
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} product`;
+  async update(id: string, dto: UpdateProductDto): Promise<Product> {
+    await this.findById(id);
+
+    const data: Partial<Product> = { ...dto };
+
+    return this.prisma.product
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.product.delete({ where: { id } });
+    throw new HttpException('', 204);
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'An error occurred while performing the operation!',
+    );
   }
 }
