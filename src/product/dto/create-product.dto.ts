9c200c6c34b3c506cac2a4311129850adc65c8f4
Gustavo Martins <gugumartin77@gmail.com>
2022-05-26 12:32:43 -0300

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'The product name.',
+    example: 'Mozzarella Pizza.',
+  })
   name: string;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'The product price.',
+    example: 12.34,
+  })
   price: number;
+  @IsString()
+  @ApiProperty({
+    description: 'The product description.',
+    example: 'Thin mozzarella cheese, thin crust and rim filled with catupiry.',
+  })
   description: string;
+  @IsUrl()
+  @ApiProperty({
+    description: 'The product image.',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
   image: string;
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProductController],
   providers: [ProductService]
 })
