7f34da2ac09caea6624ad8ed96f8dc586febea21
Gustavo Martins <gugumartin77@gmail.com>
2022-06-09 22:21:44 -0300

    feat(auth): implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
-    return {
-      token: 'teste',
-      user: undefined
+    const { nickname, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({ where: { nickname } });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid user or password!');
     }
+
+    const isHashValid = await bcrypt.compare(password, user.password);
+
+    if (!isHashValid) {
+      throw new UnauthorizedException('Invalid user or password!');
+    }
+
+    delete user.password;
+
+    return {
+      token: this.jwtService.sign({ nickname }),
+      user,
+    };
   }
 }
